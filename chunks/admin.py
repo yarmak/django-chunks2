01b6bd9459a13ce9d0ab7a5726e1420603d70900
Ruslan Popov <ruslan.popov@gmail.com>
2012-06-21 16:13:20 +0400

    Conform to PEP8.

--- chunks/admin.py
+++ chunks/admin.py
@@ -1,8 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib import admin
 from .models import Chunk
 
+
 class ChunkAdmin(admin.ModelAdmin):
-  list_display = ('key',)
-  search_fields = ('key', 'content')
+    list_display = ('key',)
+    search_fields = ('key', 'content')
 
 admin.site.register(Chunk, ChunkAdmin)
--- chunks/models.py
+++ chunks/models.py
@@ -1,36 +1,36 @@
+# -*- coding: utf-8 -*-
+
 from django.db import models
 from django.core.cache import cache
 from django.utils.translation import ugettext_lazy as _
 
-
 CACHE_PREFIX = 'chunks_'
 
+
 class Chunk(models.Model):
-    """
+    u"""
     A Chunk is a piece of content associated
     with a unique key that can be inserted into
     any template with the use of a special template
     tag
     """
-    key = models.CharField(_('key'), max_length=255, unique=True, 
-                           help_text=_("A unique name for this chunk of content"))
-    content = models.TextField(_('content'), blank=True)
-
+    key = models.CharField(_(u'key'), max_length=255, unique=True,
+                           help_text=_(u'A unique name for this chunk of content'))
+    content = models.TextField(_(u'content'), blank=True)
 
     def __unicode__(self):
         return self.key
-    
+
     def save(self, *args, **kwargs):
-        cache.delete(CACHE_PREFIX + self.key) # cache invalidation on save
+        cache.delete(CACHE_PREFIX + self.key)  # cache invalidation on save
         super(Chunk, self).save(*args, **kwargs)
-    
-    
+
     @staticmethod
     def get(key):
         cache_key = CACHE_PREFIX + key
         content = cache.get(cache_key)
         if content is None:
-            obj, c_ = Chunk.objects.get_or_create(key=key, defaults={'content': key}) 
+            obj, c_ = Chunk.objects.get_or_create(key=key, defaults={'content': key})
             cache.set(cache_key, obj.content)
             content = obj.content
         else:
