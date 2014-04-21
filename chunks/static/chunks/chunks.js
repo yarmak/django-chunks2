7c221001351d07e51ecebabc674abf23713c3d08
Ruslan Popov <ruslan.popov@gmail.com>
2014-04-21 12:16:35 +0400

    Esc and Ctrl-Enter do cancel and submit actions respectively.

--- chunks/static/chunks/chunks.js
+++ chunks/static/chunks/chunks.js
@@ -4,30 +4,40 @@
             'click',
             'a',
             function() {
-                var key = $(this).parent().data('key');
-                var chunk = $(this).parent();
+                var key = $(this).parent().data('key'),
+                    url = '/chunks/edit/' + key + '/',
+                    link = '<a href="' + url + '">редактировать</a>',
+                    chunk = $(this).parent(),
+                    original = chunk.html();
                 chunk.empty();
-                $.get('/chunks/edit/' + key + '/',
+                $.get(url,
                       {},
                       function(data) {
                           chunk.html(data)
-                          $('form', chunk).ajaxForm({
-                              dataType: 'text',
-                              beforeSubmit: function() { $('button', chunk).prop('disabled', true); },
-                              success: function(d, s, x) {
-                                  console.log('SUCCESS: ' + x.status);
-                                  $('form', chunk).css({
-                                      'border': '10px solid green',
-                                  }).animate(
-                                      {'opacity': 'hide',},
-                                      1000, 'linear',
-                                      function() {
-                                          var txt = $('.edit_chunk form textarea').val();
-                                          chunk.empty();
-                                          chunk.html(txt + '<a href="#">редактировать</a>')});
-                              },
-                              error: function(d, s, x) { console.log('ERROR: ' + x.status); }
-                          }).find('textarea').select();
+                          $('form', chunk)
+                              .ajaxForm({
+                                  dataType: 'text',
+                                  beforeSubmit: function() { $('button', chunk).prop('disabled', true); },
+                                  success: function(d, s, x) {
+                                      $('form', chunk).css({
+                                          'border': '10px solid green',
+                                      }).animate(
+                                          {'opacity': 'hide',},
+                                          1000, 'linear',
+                                          function() {
+                                              var txt = $('.edit_chunk form textarea').val();
+                                              chunk.empty().html(txt + link)});
+                                  },
+                                  error: function(d, s, x) { console.log('ERROR: ' + x.status); }})
+                              .find('textarea')
+                              .keydown(function(e) {
+                                  if (e.ctrlKey && e.keyCode == 13) {
+                                      $(this).parent().submit();
+                                  } else if (e.keyCode == 27) {
+                                      chunk.empty().html(original);
+                                  }
+                              })
+                              .select();
                       });
                 return false;
             });
