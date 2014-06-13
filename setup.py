7eccd6df3d2819745b2bc4329987f7de978d116c
gotlium <gotlium@gmail.com>
2014-06-14 02:09:02 +0500

    setup fixes + pypi

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
-from distutils.core import setup
-import setuplib
+from setuptools import setup, find_packages
 
-packages, package_data = setuplib.find_packages(['chunks', 'chunks.templatetags', 'migrations'])
-
-setup(name='chunks',
-      version='2.8',
-      description='Keyed blocks of content for use in your Django templates',
-      author='Ruslan Popov',
-      author_email='ruslan.popov@gmail.com',
-      url='https://github.com/RaD/django-chunks',
-      install_requires=['easy_thumbnails'],
-      packages=packages,
-      package_data=package_data,
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-chunks2',
+    version='2.8',
+    description='Keyed blocks of content for use in your Django templates',
+    keywords="django metrics",
+    author='Ruslan Popov',
+    author_email='ruslan.popov@gmail.com',
+    url='https://github.com/RaD/django-chunks',
+    packages=find_packages(exclude=['demo']),
+    include_package_data=True,
+    install_requires=[
+        'easy_thumbnails',
+    ],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities'
+    ],
+)
