48560ab173ccf5407fa5bf744c43b8a5ec513549
Maxim <region070@gmail.com>
2014-08-19 15:21:04 +0700

    django migrations

--- chunks/migrations/0001_initial.py
+++ chunks/migrations/0001_initial.py
@@ -1,69 +1,74 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
-from south.db import db
-from south.v2 import SchemaMigration
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Chunk'
-        db.create_table('chunks_chunk', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('content', self.gf('django.db.models.fields.TextField')(blank=True)),
-        ))
-        db.send_create_signal('chunks', ['Chunk'])
+    dependencies = [
+    ]
 
-        # Adding model 'Image'
-        db.create_table('chunks_image', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=255)),
-        ))
-        db.send_create_signal('chunks', ['Image'])
-
-        # Adding model 'Media'
-        db.create_table('chunks_media', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('key', self.gf('django.db.models.fields.CharField')(unique=True, max_length=255)),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=64)),
-            ('desc', self.gf('django.db.models.fields.CharField')(max_length=256, null=True, blank=True)),
-            ('media', self.gf('django.db.models.fields.files.FileField')(max_length=256, null=True, blank=True)),
-        ))
-        db.send_create_signal('chunks', ['Media'])
-
-    def backwards(self, orm):
-        # Deleting model 'Chunk'
-        db.delete_table('chunks_chunk')
-
-        # Deleting model 'Image'
-        db.delete_table('chunks_image')
-
-        # Deleting model 'Media'
-        db.delete_table('chunks_media')
-
-    models = {
-        'chunks.chunk': {
-            'Meta': {'ordering': "('key',)", 'object_name': 'Chunk'},
-            'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
-        },
-        'chunks.image': {
-            'Meta': {'ordering': "('key',)", 'object_name': 'Image'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '255'}),
-            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
-        },
-        'chunks.media': {
-            'Meta': {'ordering': "('key',)", 'object_name': 'Media'},
-            'desc': ('django.db.models.fields.CharField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'key': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
-            'media': ('django.db.models.fields.files.FileField', [], {'max_length': '256', 'null': 'True', 'blank': 'True'}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
-        }
-    }
-
-    complete_apps = ['chunks']
+    operations = [
+        migrations.CreateModel(
+            name='Chunk',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(help_text='A unique name for this chunk of content', unique=True, max_length=255, verbose_name='key')),
+                ('content', models.TextField(verbose_name='content', blank=True)),
+            ],
+            options={
+                'ordering': (b'key',),
+                'abstract': False,
+                'verbose_name': 'Text Chunk',
+                'verbose_name_plural': 'Text Chunks',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Group',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(help_text='A name for chunks group', max_length=255, verbose_name='key')),
+                ('content', models.TextField(verbose_name='content', blank=True)),
+            ],
+            options={
+                'ordering': (b'key',),
+                'verbose_name': 'Group Chunk',
+                'verbose_name_plural': 'Group Chunks',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(help_text='A unique name for this chunk of content', unique=True, max_length=255, verbose_name='key')),
+                ('image', models.ImageField(upload_to='chunks/images', max_length=255, verbose_name='image')),
+            ],
+            options={
+                'ordering': (b'key',),
+                'abstract': False,
+                'verbose_name': 'Image Chunk',
+                'verbose_name_plural': 'Image Chunks',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Media',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('key', models.CharField(help_text='A unique name for this chunk of content', unique=True, max_length=255, verbose_name='key')),
+                ('title', models.CharField(max_length=64, verbose_name='Title')),
+                ('desc', models.CharField(max_length=256, null=True, verbose_name='Description', blank=True)),
+                ('media', models.FileField(max_length=256, upload_to=b'chunks/media', null=True, verbose_name='Media', blank=True)),
+            ],
+            options={
+                'ordering': (b'key',),
+                'abstract': False,
+                'verbose_name': 'Media Chunk',
+                'verbose_name_plural': 'Media Chunks',
+            },
+            bases=(models.Model,),
+        ),
+    ]
